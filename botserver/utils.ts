6549dc2961224aa57e1b8385b9495f50a6337b87
Garby Exe <75593400+Garbygas@users.noreply.github.com>
2023-02-04 13:57:37 -0600

    reorganize

--- botserver/utils.ts
+++
@@ -1,4 +0,0 @@
-const a = { apple: ["a", "b"], banana: ["c", "d"] };
-
-const g = "banana";
-console.log(a[g]);
--- types/comman/index.d.ts
+++
@@ -1,5 +0,0 @@
-class nClient extends Client {
-  commands: Collection<unknown, any>;
-}
-
-export { nClient };
